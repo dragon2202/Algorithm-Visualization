a6244682d2be4bb535167956bc03c21e27743df0
Calvin Tang <tcalvin9@gmail.com>
2022-07-14 14:36:17 -0400

    Update prim.tsx
    Added a loop. Previously to apply the concept, I added just the start node to grab the minimum edge connected to start node. This update allows me to monitor mutiple loops to see if the logic is correct after one, two, and three runs.

--- components/cytograph/algorithm/prim.tsx
+++ components/cytograph/algorithm/prim.tsx
@@ -1,43 +1,40 @@
 import CytographProps from '../../props/cytograph_master_props'
 
 export default function Prim (props: CytographProps) {
-    let startingNode = props.nodes[0].data.id//first node in array is default starting point
     let visited: any = []//keep track of visited nodes to prevent cycles
     visited.push(props.nodes[0].data.id)
-    let MinimumSpanningTree_Edges = []
-    let edgesToBeConsidered: any= []
-    
-    const edgesConnected = props.edges.filter((item: any) => item.data.source === startingNode || item.data.target === startingNode)
+    let MinimumSpanningTree_Edges: any = []
+    for (let i = 0; i < 3; i++) {
+        let edgesToBeConsidered: any = []
+        for (let j = 0; j < visited.length; j++) {
+            const edgesConnected = props.edges.filter((item: any) => item.data.source === visited[j] || item.data.target === visited[j])
+            edgesToBeConsidered.push(...edgesConnected)
+        }
+        //removes duplicates. For example if a connects to b. next run b gets all connected nodes including a, there will be two a to b connections
+        edgesToBeConsidered = Array.from(new Set(edgesToBeConsidered))
+        //removes edges that are already added to the minimum tree, and removes it from consideration.
+        edgesToBeConsidered = edgesToBeConsidered.filter((val: any) => !MinimumSpanningTree_Edges.includes(val))
 
-    edgesToBeConsidered.push(...edgesConnected)
-    const weightsToBeConsidered = edgesToBeConsidered.map((item: any) => parseInt(item.data.weight))
-    const min = Math.min.apply(null, weightsToBeConsidered)
+        const weightsToBeConsidered = edgesToBeConsidered.map((item: any) => parseInt(item.data.weight))
+        const min = Math.min.apply(null, weightsToBeConsidered)
 
-    const indexesOfAllMinNum = []
-    for (let i = 0; i < weightsToBeConsidered.length; i++) {
-        if(weightsToBeConsidered[i] === min) {
-            indexesOfAllMinNum.push(i)
+        const indexesOfAllMinNum = []
+        for (let i = 0; i < weightsToBeConsidered.length; i++) {
+            if(weightsToBeConsidered[i] === min) {
+                indexesOfAllMinNum.push(i)
+            }
+        }
+
+        for (let i = 0; i < indexesOfAllMinNum.length; i++) {
+            if(visited.includes((edgesToBeConsidered[indexesOfAllMinNum[i]].data.source))) {
+                visited.push(edgesToBeConsidered[indexesOfAllMinNum[i]].data.target)
+                MinimumSpanningTree_Edges.push(edgesToBeConsidered[indexesOfAllMinNum[i]])
+            } else {
+                visited.push(edgesToBeConsidered[indexesOfAllMinNum[i]].data.source)
+                MinimumSpanningTree_Edges.push(edgesToBeConsidered[indexesOfAllMinNum[i]])
+            }
         }
     }
-
-    for (let i = 0; i < indexesOfAllMinNum.length; i++) {
-        if(visited.includes(parseInt(edgesToBeConsidered[indexesOfAllMinNum[i]].data.source))) {
-            visited.push(edgesToBeConsidered[indexesOfAllMinNum[i]].data.target)
-            MinimumSpanningTree_Edges.push(edgesToBeConsidered[indexesOfAllMinNum[i]])
-        } else {
-            visited.push(edgesToBeConsidered[indexesOfAllMinNum[i]].data.target)
-            MinimumSpanningTree_Edges.push(edgesToBeConsidered[indexesOfAllMinNum[i]])
-        }
-    }
-    console.log(min)
-    console.log(weightsToBeConsidered)
-
-    console.log(edgesToBeConsidered)
-    console.log(visited)
     console.log(MinimumSpanningTree_Edges)
-}
-/*
-    while (visited.length !== props.nodes.length) {
 
-    }
-*/
+}
