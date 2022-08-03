0c85f1ee5f87090e8078a5e88752fc308c8c1b7f
Calvin Tang <tcalvin9@gmail.com>
2022-08-03 15:01:22 -0400

    Minor updates
    Minor tinkering with a bar chart for a hashtable visualization

--- components/chart/hashtable.tsx
+++ components/chart/hashtable.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Bar } from 'react-chartjs-2'
 import { Chart, Tooltip, CategoryScale, LinearScale, Title, BarElement } from 'chart.js'
 Chart.register(Tooltip, CategoryScale, LinearScale, Title, BarElement)
@@ -15,10 +15,26 @@ import { queueText } from './text/queue-text'
 
 const arrayLength = 7//bucket size
 
+///table[i][j][k]
+//i is the 7 buckets
+//j is array within buckets that store items assigned by hash and items that collided
+//k is the array that stores keys and values
+
 export default function HashtableComponent() {
     const [table, setTable] = useState<Array<any>>(new Array(arrayLength))
+    const [labels, setLabels] = useState<Array<string>>([])
     const [size, setSize] = useState<number>(0)
+    const [dataSets, setDataSets] = useState<Array<any>>([])
 
+    useEffect(()=> {
+        const array: Array<string> = []
+        for(let i = 0; i < arrayLength; i++) {
+            const label = "Bucket " + (i + 1)
+            array.push(label)
+        }
+        setLabels(array)
+    },[])
+    
     function hash(key: string) {
         let hash = 0
         for (let i = 0; i< key.length; i++){
@@ -27,19 +43,35 @@ export default function HashtableComponent() {
         return hash % arrayLength
     }
 
+    function setHashTable(key: string, value: any){
+        const array = Array(arrayLength).fill(0)
+        array[dataSets.length % arrayLength] = 1
+        const item = {
+            label: "Item " + (dataSets.length + 1),
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 1,
+            data: array
+        }
+        setDataSets([...dataSets, item])
+    }
+
     function set(key: string, value: any) {
         const index = hash(key)
         if(table[index] !== undefined) {
             for(let i = 0; i < table[index].length; i++) {
                 //Find key/value pair in the chain
+                console.log('inside')
                 if(table[index][i][0] === key){
                     table[index][i][1] = value
                     return
                 }
             }
+            console.log('outside')
             //not found, push a new key/value pair
             table[index].push([key, value])
         } else {
+            console.log('else')
             table[index] = []
             table[index].push([key, value])
         }
@@ -68,21 +100,55 @@ export default function HashtableComponent() {
 
     function test() {
         set("Spain", 110)
-        set("ǻ", 192)
+        console.log(get('Spain'))
+        //set("Spain", 110)
+        //set("ǻ", 192)
     }
     function test2() {
-        console.log(get("Spain"))
-        console.log(get("ǻ"))
+        console.log(table)
+        console.log(table[3][0][0])
+        console.log(table[3][0][1])
     }
 
     return (
-        <div className="buttongroup">
-            <ButtonGroup orientation="vertical" variant="contained" className="buttongroup">
-                <Button key="one" className="enqueue" onClick={() => console.log(table)}>Print</Button>
-                <Button key="two" className="enqueue" onClick={() => set('1', 'Fart')}>Set</Button>
-                <Button key="three" className="enqueue" onClick={() => test()}>Test</Button>
-                <Button key="four" className="enqueue" onClick={() => test2()}>Test2</Button>
-            </ButtonGroup>
+        <div>
+            <Card className="card">
+                <Bar 
+                    className="bar"
+                    data={{ labels: labels, datasets: [...dataSets] }}
+                    options={{
+                        indexAxis: 'y',
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: 'Items in a Queue',
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            },
+
+                        },
+                        scales: {
+                            x: {
+                                stacked: true,
+                            },
+                            y: {
+                                stacked: true
+                            }
+                        }
+                    }}
+                />
+            </Card>
+            <div className="buttongroup">
+                <ButtonGroup orientation="vertical" variant="contained" className="buttongroup">
+                    <Button key="one" className="enqueue" onClick={() => console.log(table)}>Print</Button>
+                    <Button key="two" className="enqueue" onClick={() => set('1', 'Fart')}>Set</Button>
+                    <Button key="three" className="enqueue" onClick={() => test()}>Test</Button>
+                    <Button key="four" className="enqueue" onClick={() => test2()}>Test2</Button>
+                </ButtonGroup>
+            </div>
         </div>
+
     )
 }
